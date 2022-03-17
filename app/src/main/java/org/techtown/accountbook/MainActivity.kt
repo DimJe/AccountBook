b37890979867ea131fda6c18f9b8434473224d45
Dimje <mark759@naver.com>
2022-03-17 17:31:25 +0900

    가계부

--- app/src/main/java/org/techtown/accountbook/MainActivity.kt
+++ app/src/main/java/org/techtown/accountbook/MainActivity.kt
@@ -1,14 +1,22 @@
 package org.techtown.accountbook
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    companion object{
+        val TAG: String = "로그"
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val text = findViewById<TextView>(R.id.text)
-        text.text = "hih"
+        btn.setOnClickListener {
+            val intent = Intent(this,AddActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
