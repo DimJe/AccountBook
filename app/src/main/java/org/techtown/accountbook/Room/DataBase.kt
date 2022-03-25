e83fd510b5be6a810e70096ff5991b5e6b758a1b
Dimje <mark759@naver.com>
2022-03-26 03:12:16 +0900

    .

--- app/src/main/java/org/techtown/accountbook/Room/DataBase.kt
+++ app/src/main/java/org/techtown/accountbook/Room/DataBase.kt
@@ -5,7 +5,7 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "account")
-class Item(
+data class Item(
     @PrimaryKey(autoGenerate = true) var id : Int?,
     @ColumnInfo(name = "year") var year : String?,
     @ColumnInfo(name = "month") var month : String?,
