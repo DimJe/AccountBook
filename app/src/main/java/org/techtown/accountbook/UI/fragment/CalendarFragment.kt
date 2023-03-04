a3de642a2f7a629511c83a3f87e174f81692f3ce
Dimje <mark759@naver.com>
2023-03-04 22:03:29 +0900

    [FEAT] - RecyclerView Swipe Listener 구현 좌우 Swipe에 따른 calendar 객체 Month 조절

--- app/src/main/java/org/techtown/accountbook/UI/fragment/CalendarFragment.kt
+++ app/src/main/java/org/techtown/accountbook/UI/fragment/CalendarFragment.kt
@@ -9,6 +9,7 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.PagerSnapHelper
+import androidx.recyclerview.widget.RecyclerView
 import org.techtown.accountbook.R
 import org.techtown.accountbook.adapter.MonthAdapter
 import org.techtown.accountbook.databinding.FragmentCalendarBinding
@@ -16,13 +17,12 @@ import java.util.*
 
 
 class CalendarFragment : Fragment() {
-    // TODO: Rename and change types of parameters
     private var _binding: FragmentCalendarBinding? = null
     private val binding get() = _binding!!
-
+    private val calender = Calendar.getInstance()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        calender.time = Date()
     }
 
     override fun onCreateView(
@@ -39,6 +39,9 @@ class CalendarFragment : Fragment() {
 
     private fun initView(){
         binding.calendarRecyclerView.apply {
+            val psHelper = PagerSnapHelper()
+            psHelper.attachToRecyclerView(this)
+
             layoutManager = LinearLayoutManager(requireContext(),LinearLayoutManager.HORIZONTAL,false)
             adapter = MonthAdapter().apply {
                 setOnItemClickListener(object : MonthAdapter.OnItemClickListener{
@@ -47,12 +50,42 @@ class CalendarFragment : Fragment() {
                             time = date
                         }.get(Calendar.DAY_OF_MONTH)}", Toast.LENGTH_SHORT).show()
                     }
-
                 })
             }
             scrollToPosition(Int.MAX_VALUE/2)
+            addOnScrollListener(object : RecyclerView.OnScrollListener(){
+                var temp = 0
+                var dir = false
+                var currentPos = RecyclerView.NO_POSITION
+                override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                    if(temp == 0){
+                        super.onScrolled(recyclerView, dx, dy)
+                        if(dx==0) return
+                        val view = psHelper.findSnapView(recyclerView.layoutManager)!!
+                        currentPos = recyclerView.layoutManager!!.getPosition(view)
+                        temp = 1
+                    }
+
+                }
+                override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                    if(newState==RecyclerView.SCROLL_STATE_IDLE && temp == 1){
+                        super.onScrollStateChanged(recyclerView, newState)
+                        val view = psHelper.findSnapView(recyclerView.layoutManager)!!
+                        val position = recyclerView.layoutManager!!.getPosition(view)
+                        if(currentPos>position) {
+                            calender.add(Calendar.MONTH,-1)
+                            Log.d("태그", "onScrollStateChanged: ${calender.get(Calendar.YEAR)} ${calender.get(Calendar.MONTH)+1}월 ")
+                        }
+                        else if(currentPos<position){
+                            calender.add(Calendar.MONTH,1)
+                            Log.d("태그", "onScrollStateChanged:${calender.get(Calendar.YEAR)} ${calender.get(Calendar.MONTH)+1}월 ")
+                        }
+                        else Log.d("태그", "onScrollStateChanged: 같은 월 ")
+                        temp = 0
+                    }
+                }
+            })
         }
-        PagerSnapHelper().attachToRecyclerView(binding.calendarRecyclerView)
     }
     companion object {
 
