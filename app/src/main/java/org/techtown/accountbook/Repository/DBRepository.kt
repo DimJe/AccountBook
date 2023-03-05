3798c96868c3aa43e2f106ef41694d7f874563b5
Dimje <mark759@naver.com>
2023-03-05 22:47:04 +0900

    FEAT: ADD repository,viewmodel koin 모듈 생성을 위한 객체 틀만 작성함

---
+++ app/src/main/java/org/techtown/accountbook/Repository/DBRepository.kt
@@ -0,0 +1,6 @@
+package org.techtown.accountbook.Repository
+
+import org.techtown.accountbook.Model.SpendingDataDao
+
+class DBRepository(val dao: SpendingDataDao) {
+}
---
+++ app/src/main/java/org/techtown/accountbook/ViewModel/ViewModel.kt
@@ -0,0 +1,6 @@
+package org.techtown.accountbook.ViewModel
+
+import org.techtown.accountbook.Repository.DBRepository
+import androidx.lifecycle.ViewModel
+class ViewModel(val dbRepository: DBRepository) : ViewModel() {
+}
