c041535b1b249cd71a3de9d0a4bd15fd25899237
Dimje <mark759@naver.com>
2023-04-17 22:52:19 +0900

    feat : flow를 반환하는 function 작성 및 통계를 위한 데이터 클래스 작성

---
+++ app/src/main/java/org/techtown/accountbook/Model/SpendingChartingData.kt
@@ -0,0 +1,8 @@
+package org.techtown.accountbook.Model
+
+import androidx.room.ColumnInfo
+
+data class SpendingChartingData(
+    @ColumnInfo(name = "day") val day:Int,
+    @ColumnInfo(name = "money") val money:Int
+)
--- app/src/main/java/org/techtown/accountbook/Model/SpendingDataDao.kt
+++ app/src/main/java/org/techtown/accountbook/Model/SpendingDataDao.kt
@@ -31,4 +31,12 @@ interface SpendingDataDao {
             "WHERE year =:year AND month =:month\n" +
             "group by day;")
     suspend fun getUiData(year: Int,month: Int) : List<SpendingUiData>
+
+    @Query("SELECT year,month,day,sum(money) as money\n" +
+            "FROM spending \n" +
+            "WHERE year =:year AND month =:month\n" +
+            "group by day;")
+    fun getUiDataFlow(year: Int,month: Int) : Flow<List<SpendingUiData>>
+
+    //차트 그릴떄 필요한 데이터 덩는 쿼리 만들어야함 해당 월의 날짜와 금액 ex) (1,3000),(2,54440)....
 }
