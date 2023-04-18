2c8bb6e0fb53f9da2dbe3d5835164227fd994d77
dimje <mark759@naver.com>
2023-04-18 13:59:30 +0900

    feat : 막대 그래프 초기 설정 및 StateFlow 수집

--- app/src/main/java/org/techtown/accountbook/UI/fragment/StatsFragment.kt
+++ app/src/main/java/org/techtown/accountbook/UI/fragment/StatsFragment.kt
@@ -1,36 +1,171 @@
 package org.techtown.accountbook.UI.fragment
 
+import android.annotation.SuppressLint
+import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import com.github.mikephil.charting.charts.BarChart
+import com.github.mikephil.charting.components.Description
+import com.github.mikephil.charting.components.Legend
+import com.github.mikephil.charting.components.XAxis
+import com.github.mikephil.charting.components.YAxis
+import com.github.mikephil.charting.data.BarData
+import com.github.mikephil.charting.data.BarDataSet
+import com.github.mikephil.charting.data.BarEntry
+import kotlinx.coroutines.launch
+import org.koin.androidx.viewmodel.ext.android.activityViewModel
+import org.techtown.accountbook.Model.SpendingChartingData
 import org.techtown.accountbook.R
+import org.techtown.accountbook.Repository.ResultState
+import org.techtown.accountbook.UI.custom.OnSwipeTouchListener
+import org.techtown.accountbook.ViewModel.ViewModel
+import org.techtown.accountbook.databinding.FragmentStatsBinding
+import java.util.Calendar
+import java.util.Date
 
 class StatsFragment : Fragment() {
 
+    private var _binding : FragmentStatsBinding? = null
+    private val binding get() = _binding!!
+    private val viewModel: ViewModel by activityViewModel()
+    private val calender = Calendar.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        Log.d("태그", "stats onCreate: called")
         super.onCreate(savedInstanceState)
+        calender.time = Date()
 
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        Log.d("태그", "stats onCreateView: called")
-        return inflater.inflate(R.layout.fragment_stats, container, false)
+    ): View {
+
+        _binding = FragmentStatsBinding.inflate(inflater,container,false)
+        initView()
+        return binding.root
     }
 
-    companion object {
-
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            StatsFragment().apply {
-
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED){
+                viewModel.spendingChartingData.collect{
+                    when(it){
+                        is ResultState.Error -> {
+                            Log.d("태그", "onViewCreated:${it.message} ")
+                        }
+                        is ResultState.Loading -> {}
+                        is ResultState.Success -> {
+                            it.data?.let {list ->
+                                list.forEach {
+                                    Log.d("태그", "charting:$it ")
+                                }
+                                setData(binding.chart,list)
+                            }
+                        }
+                    }
+                }
             }
+        }
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    private fun initView(){
+        Log.d("태그", "stats called")
+        initBarChart(binding.chart)
+        viewModel.getChartingData(calender.get(Calendar.YEAR),calender.get(Calendar.MONTH)+1)
+
+        binding.chart.setOnTouchListener(object : OnSwipeTouchListener(requireContext()){
+            override fun onSwipeLeft() {
+                Toast.makeText(requireContext(),"왼쪽으로",Toast.LENGTH_SHORT).show()
+            }
+            override fun onSwipeRight() {
+                Toast.makeText(requireContext(),"오른쪽으로",Toast.LENGTH_SHORT).show()
+            }
+        })
+    }
+
+    private fun initBarChart(barChart: BarChart){
+        barChart.setDrawGridBackground(false)
+        // 막대 그림자 설정 (default = false)
+        barChart.setDrawBarShadow(false)
+        // 차트 테두리 설정 (default = false)
+        barChart.setDrawBorders(false)
+
+        val description = Description()
+        // 오른쪽 하단 모서리 설명 레이블 텍스트 표시 (default = false)
+        description.isEnabled = false
+        barChart.description = description
+
+        // X, Y 바의 애니메이션 효과
+        barChart.animateY(1000)
+        barChart.animateX(1000)
+
+        // 바텀 좌표 값
+        val xAxis: XAxis = barChart.xAxis
+        // x축 위치 설정
+        xAxis.position = XAxis.XAxisPosition.BOTTOM
+        // 그리드 선 수평 거리 설정
+        xAxis.granularity = 1f
+        // x축 텍스트 컬러 설정
+        xAxis.textColor = Color.RED
+        // x축 선 설정 (default = true)
+        xAxis.setDrawAxisLine(false)
+        // 격자선 설정 (default = true)
+        xAxis.setDrawGridLines(false)
+
+        val leftAxis: YAxis = barChart.axisLeft
+        // 좌측 선 설정 (default = true)
+        leftAxis.setDrawAxisLine(false)
+        // 좌측 텍스트 컬러 설정
+        leftAxis.textColor = Color.BLUE
+
+        val rightAxis: YAxis = barChart.axisRight
+        // 우측 선 설정 (default = true)
+        rightAxis.setDrawAxisLine(false)
+        // 우측 텍스트 컬러 설정
+        rightAxis.textColor = Color.GREEN
+
+        // 바차트의 타이틀
+        val legend: Legend = barChart.legend
+        // 범례 모양 설정 (default = 정사각형)
+        legend.form = Legend.LegendForm.LINE
+        // 타이틀 텍스트 사이즈 설정
+        legend.textSize = 20f
+        // 타이틀 텍스트 컬러 설정
+        legend.textColor = Color.BLACK
+        // 범례 위치 설정
+        legend.verticalAlignment = Legend.LegendVerticalAlignment.BOTTOM
+        legend.horizontalAlignment = Legend.LegendHorizontalAlignment.CENTER
+        // 범례 방향 설정
+        legend.orientation = Legend.LegendOrientation.HORIZONTAL
+        // 차트 내부 범례 위치하게 함 (default = false)
+        legend.setDrawInside(false)
+    }
+    private fun setData(barChart: BarChart,data: List<SpendingChartingData>){
+        barChart.setScaleEnabled(false)
+
+        val valueList = ArrayList<BarEntry>()
+        val title = "일별 사용 금액"
+
+        data.forEach {
+            valueList.add(BarEntry(it.day.toFloat(),it.money.toFloat()))
+        }
+
+        val barDataSet = BarDataSet(valueList,title)
+        barDataSet.color = Color.BLACK
+
+        val data = BarData(barDataSet)
+        barChart.data = data
+        barChart.invalidate()
     }
 }
