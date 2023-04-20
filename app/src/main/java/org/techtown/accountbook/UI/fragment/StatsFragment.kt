71353bb1ad625965c5d9a23f462a97ecfad9f71e
dimje <mark759@naver.com>
2023-04-20 15:39:22 +0900

    feat : Paging 데이터 collect하고 adapter에 submit

--- app/src/main/java/org/techtown/accountbook/UI/fragment/StatsFragment.kt
+++ app/src/main/java/org/techtown/accountbook/UI/fragment/StatsFragment.kt
@@ -8,10 +8,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import android.widget.Toast
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
+import androidx.recyclerview.widget.DividerItemDecoration
 import com.github.mikephil.charting.charts.BarChart
 import com.github.mikephil.charting.components.Description
 import com.github.mikephil.charting.components.Legend
@@ -20,6 +22,7 @@ import com.github.mikephil.charting.components.YAxis
 import com.github.mikephil.charting.data.BarData
 import com.github.mikephil.charting.data.BarDataSet
 import com.github.mikephil.charting.data.BarEntry
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 import org.koin.androidx.viewmodel.ext.android.activityViewModel
 import org.techtown.accountbook.Model.SpendingChartingData
@@ -27,6 +30,7 @@ import org.techtown.accountbook.R
 import org.techtown.accountbook.Repository.ResultState
 import org.techtown.accountbook.UI.custom.OnSwipeTouchListener
 import org.techtown.accountbook.ViewModel.ViewModel
+import org.techtown.accountbook.adapter.SpendingAdapter
 import org.techtown.accountbook.databinding.FragmentStatsBinding
 import java.util.Calendar
 import java.util.Date
@@ -37,6 +41,7 @@ class StatsFragment : Fragment() {
     private val binding get() = _binding!!
     private val viewModel: ViewModel by activityViewModel()
     private val calender = Calendar.getInstance()
+    lateinit var adapter: SpendingAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -58,20 +63,28 @@ class StatsFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         viewLifecycleOwner.lifecycleScope.launch {
             viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED){
-                viewModel.spendingChartingData.collect{
-                    when(it){
-                        is ResultState.Error -> {
-                            Log.d("태그", "onViewCreated:${it.message} ")
-                        }
-                        is ResultState.Loading -> {}
-                        is ResultState.Success -> {
-                            it.data?.let {list ->
-                                list.forEach {
-                                    Log.d("태그", "charting:$it ")
+                launch {
+                    viewModel.spendingChartingData.collect{
+                        when(it){
+                            is ResultState.Error -> {
+                                Log.d("태그", "onViewCreated:${it.message} ")
+                            }
+                            is ResultState.Loading -> {}
+                            is ResultState.Success -> {
+                                it.data?.let {list ->
+                                    list.forEach {
+                                        Log.d("태그", "charting:$it ")
+                                    }
+                                    setData(binding.chart,list)
+                                    setTitle(list)
                                 }
-                                setData(binding.chart,list)
                             }
                         }
+                    }
+                }
+                launch {
+                    viewModel.getPagingData(calender.get(Calendar.YEAR),calender.get(Calendar.MONTH)+1).collectLatest {
+                        adapter.submitData(it)
                     }
                 }
             }
@@ -92,6 +105,10 @@ class StatsFragment : Fragment() {
                 Toast.makeText(requireContext(),"오른쪽으로",Toast.LENGTH_SHORT).show()
             }
         })
+
+        adapter = SpendingAdapter()
+        binding.spendingList.adapter = adapter
+        binding.spendingList.addItemDecoration(DividerItemDecoration(requireContext(),LinearLayout.VERTICAL))
     }
 
     private fun initBarChart(barChart: BarChart){
@@ -127,13 +144,13 @@ class StatsFragment : Fragment() {
         // 좌측 선 설정 (default = true)
         leftAxis.setDrawAxisLine(false)
         // 좌측 텍스트 컬러 설정
-        leftAxis.textColor = Color.BLUE
+        leftAxis.textColor = Color.BLACK
 
         val rightAxis: YAxis = barChart.axisRight
         // 우측 선 설정 (default = true)
         rightAxis.setDrawAxisLine(false)
         // 우측 텍스트 컬러 설정
-        rightAxis.textColor = Color.GREEN
+        rightAxis.textColor = Color.BLACK
 
         // 바차트의 타이틀
         val legend: Legend = barChart.legend
@@ -167,5 +184,15 @@ class StatsFragment : Fragment() {
         val data = BarData(barDataSet)
         barChart.data = data
         barChart.invalidate()
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun setTitle(list: List<SpendingChartingData>){
+
+        val total = list.sumOf { it.money }
+        val max = list.maxBy { it.money }.money
+
+        binding.totalCount.text = "${calender.get(Calendar.MONTH)+1}월 총 금액 : $total"
+        binding.highestSpending.text = "${calender.get(Calendar.MONTH)+1}월 최고 금액 : $max"
     }
 }
