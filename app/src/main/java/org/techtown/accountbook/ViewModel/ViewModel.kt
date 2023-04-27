a215ac4e4272d9df1674097b5a884840e3ea0162
dimje <mark759@naver.com>
2023-04-27 17:08:40 +0900

    feat: 페이징 데이터 가져오는 함수 작성

--- app/src/main/java/org/techtown/accountbook/ViewModel/ViewModel.kt
+++ app/src/main/java/org/techtown/accountbook/ViewModel/ViewModel.kt
@@ -6,9 +6,11 @@ import androidx.lifecycle.viewModelScope
 import androidx.paging.PagingData
 import androidx.paging.cachedIn
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.launch
 import org.techtown.accountbook.Model.SpendingChartingData
 import org.techtown.accountbook.Model.SpendingData
@@ -20,11 +22,16 @@ class ViewModel(private val dbRepository: DBRepository) : ViewModel() {
     private var mSpendingUiData: MutableStateFlow<ResultState<List<SpendingUiData>>> = MutableStateFlow(ResultState.Loading())
     var spendingUiData: StateFlow<ResultState<List<SpendingUiData>>> = mSpendingUiData
 
-    private var mSpendingData: MutableStateFlow<ResultState<List<SpendingData>>> = MutableStateFlow(ResultState.Loading())
-    var spendingData: StateFlow<ResultState<List<SpendingData>>> = mSpendingData
 
     private var mSpendingChartingData: MutableStateFlow<ResultState<List<SpendingChartingData>>> = MutableStateFlow(ResultState.Loading())
     var spendingChartingData: StateFlow<ResultState<List<SpendingChartingData>>> = mSpendingChartingData
+
+    private val searchPagingFlow = MutableSharedFlow<Pair<Int,Int>>()
+    val pagingDataFlow  = searchPagingFlow
+        .flatMapLatest {
+            getPagingData(it.first,it.second)
+        }
+        .cachedIn(viewModelScope)
 
     fun insertData(data: SpendingData)= viewModelScope.launch {
         dbRepository.insertSpendingData(data)
@@ -51,8 +58,14 @@ class ViewModel(private val dbRepository: DBRepository) : ViewModel() {
             }
     }
 
-    fun getPagingData(year: Int,month: Int) : Flow<PagingData<SpendingData>>{
-        return dbRepository.getPagingData(year, month).cachedIn(viewModelScope)
+    private fun getPagingData(year: Int, month: Int) : Flow<PagingData<SpendingData>>{
+        return dbRepository.getPagingData(year, month)
+    }
+    fun requestPagingData(year: Int,month: Int){
+
+        viewModelScope.launch {
+            searchPagingFlow.emit(Pair(year,month))
+        }
     }
     fun deleteDB(){
         viewModelScope.launch {
