974149dfa172e632a6d36c72eae04f11c7b2ddd5
dimje <mark759@naver.com>
2023-04-27 17:10:13 +0900

    modify: 필요없는 코드 삭제

--- app/src/main/java/org/techtown/accountbook/Repository/DBRepository.kt
+++ app/src/main/java/org/techtown/accountbook/Repository/DBRepository.kt
@@ -1,31 +1,20 @@
 package org.techtown.accountbook.Repository
 
-import android.util.Log
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.PagingData
-import androidx.paging.cachedIn
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
 import org.techtown.accountbook.Model.SpendingData
 import org.techtown.accountbook.Model.SpendingDataDao
-import org.techtown.accountbook.Model.SpendingUiData
 
 class DBRepository(private val dao: SpendingDataDao) {
 
     suspend fun insertSpendingData(data: SpendingData){
         dao.insertData(data)
     }
-    fun getUiData(year: Int,month: Int): Flow<ResultState<List<SpendingUiData>>> = flow{
-        try {
-            emit(ResultState.Success(dao.getUiData(year,month)))
-        }catch (e:Exception){
-            emit(ResultState.Error(e.message?:""))
-        }
-    }.flowOn(Dispatchers.IO)
-
     fun getUiDataFlow(year: Int,month: Int) = flow{
         dao.getUiDataFlow(year, month).collect{
             emit(ResultState.Success(it))
@@ -39,6 +28,7 @@ class DBRepository(private val dao: SpendingDataDao) {
     }.flowOn(Dispatchers.IO)
 
     fun getPagingData(year: Int,month: Int): Flow<PagingData<SpendingData>>{
+
         return Pager(config = PagingConfig(
             pageSize = 10,
             enablePlaceholders =  false,
