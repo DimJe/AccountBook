aaca4f6f66b0b58779ab85755aff65885a4f1b68
dimje <mark759@naver.com>
2024-01-29 18:05:53 +0900

    feat : timber 라이브러리 실행

--- app/src/main/java/org/techtown/accountbook/DI/App.kt
+++ app/src/main/java/org/techtown/accountbook/DI/App.kt
@@ -3,6 +3,7 @@ package org.techtown.accountbook.DI
 import android.app.Application
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
+import timber.log.Timber
 
 class App: Application() {
 
@@ -11,6 +12,7 @@ class App: Application() {
         startKoin{
             androidContext(this@App)
             modules(module)
+            Timber.plant(Timber.DebugTree())
         }
     }
 }
