b141b4c8a3117a2f66b297687ca2563f5a7ad212
Dimje <mark759@naver.com>
2024-02-10 14:01:09 +0900

    임시커밋

--- app/src/main/java/org/techtown/accountbook/UI/activity/MainActivity.kt
+++ app/src/main/java/org/techtown/accountbook/UI/activity/MainActivity.kt
@@ -10,11 +10,18 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.Settings
 import androidx.core.app.NotificationManagerCompat
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
 import org.koin.android.ext.android.inject
 import org.techtown.accountbook.R
 import org.techtown.accountbook.Service.MyNotificationListenerService
 import org.techtown.accountbook.UI.fragment.CalendarFragment
+import org.techtown.accountbook.UI.fragment.CalendarFragmentDirections
 import org.techtown.accountbook.UI.fragment.StatsFragment
+import org.techtown.accountbook.UI.fragment.StatsFragmentDirections
 import org.techtown.accountbook.ViewModel.ViewModel
 import org.techtown.accountbook.databinding.ActivityMainBinding
 import timber.log.Timber
@@ -33,7 +40,9 @@ class MainActivity : AppCompatActivity() {
     private val calendarFragment = CalendarFragment()
     private val statsFragment = StatsFragment()
     private val viewModel: ViewModel by inject()
-
+    private val navController: NavController by lazy {
+        (supportFragmentManager.findFragmentById(R.id.navhost) as NavHostFragment).navController
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
@@ -52,31 +61,19 @@ class MainActivity : AppCompatActivity() {
     }
     private fun initView(){
 
-        supportFragmentManager
-            .beginTransaction()
-            .replace(R.id.fragment,calendarFragment)
-            .commit()
-        supportFragmentManager
-            .beginTransaction().add(R.id.fragment,statsFragment).hide(statsFragment).commit()
         binding.chart.hideText()
+
+
         binding.chart.setOnClickListener {
 
-            //replace로 fragment전환을 하면 계속 새롭게 생성된다.
-            //show와 hide를 사용해서 이를 방지한다.
-            supportFragmentManager
-                .beginTransaction().setCustomAnimations(R.anim.from_right,R.anim.to_left).hide(calendarFragment).commit()
-            supportFragmentManager
-                .beginTransaction().setCustomAnimations(R.anim.from_right,R.anim.to_left).show(statsFragment).commit()
+            navController.navigate(CalendarFragmentDirections.actionCalendarFragmentToStatsFragment())
 
             binding.chart.showText()
             binding.calendar.hideText()
         }
         binding.calendar.setOnClickListener {
 
-            supportFragmentManager
-                .beginTransaction().setCustomAnimations(R.anim.from_left,R.anim.to_right).hide(statsFragment).commit()
-            supportFragmentManager
-                .beginTransaction().setCustomAnimations(R.anim.from_left,R.anim.to_right).show(calendarFragment).commit()
+            navController.navigate(StatsFragmentDirections.actionStatsFragmentToCalendarFragment())
 
             binding.chart.hideText()
             binding.calendar.showText()
